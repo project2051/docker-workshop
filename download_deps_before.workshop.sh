6dcdcfe8b89e5c5fabe799feb9dccdf2ddf38e10
mieciu <przemyslaw.hejman@gmail.com>
2014-09-24 15:19:43 +0200

    Update download_deps_before.workshop.sh

--- download_deps_before.workshop.sh
+++ download_deps_before.workshop.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+
+docker pull ubuntu
+docker pull redis
+
 # Just browse throught the directories and build all the Dockerfiles found.
 for directory in `find . -name Dockerfile -exec dirname {} \;` ;do
 	cd ${directory}
