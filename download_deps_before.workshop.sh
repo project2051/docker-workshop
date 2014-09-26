002c4db8603d7aec44095a8b71b21650392fed8a
3h4x <ochach@gmail.com>
2014-09-26 21:05:51 +0200

    Update download_deps_before.workshop.sh

--- download_deps_before.workshop.sh
+++ download_deps_before.workshop.sh
@@ -2,8 +2,7 @@
 
 # Just browse throught the directories and build all the Dockerfiles found.
 for directory in `find . -name Dockerfile -exec dirname {} \;` ;do
-	cd ${directory}
-	docker build .
+	docker build ${directory}
 done 
 
 #Do the same with fig
